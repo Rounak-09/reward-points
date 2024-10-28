90b155114282b4774cf25a7f78d88de56415846f
Rounak Abraham <rounak.abraham@ad.infosys.com>
2024-10-28 13:12:30 +0530

    created global exception handler

---
+++ rewardPoints/src/main/java/com/walmart/rewardPoints/exception/SystemException.java
@@ -0,0 +1,12 @@
+package com.walmart.rewardPoints.exception;
+
+public class SystemException extends RuntimeException {
+
+    public SystemException() {
+
+    }
+
+    public SystemException(String message) {
+        super(message);
+    }
+}
