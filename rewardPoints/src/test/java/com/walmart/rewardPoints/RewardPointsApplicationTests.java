76671d299d0d9b435a2ad55f3e7aac19e6f593d4
Rounak Abraham <rounak.abraham@ad.infosys.com>
2024-10-28 10:45:54 +0530

    project setup - created from spring initializr

---
+++ rewardPoints/src/test/java/com/walmart/rewardPoints/RewardPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.rewardPoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
